6b2fe4b7423187278d64c7a017b7bc096f7717eb
Shawn Martin-Truesdell <shawn@masyc.com>
2020-11-09 07:30:45 -0800

    Initial commit of config.org and init.el

---
+++ init.el
@@ -0,0 +1,26 @@
+;;; init.el --- GNU Emacs Configuration
+
+;;; Commentary:
+
+;; Following lines build the configuration code out of the config.el file.
+
+;;; Code:
+
+;; Make startup faster by reducing the frequency of garbage
+;; collection.
+(setq gc-cons-threshold (* 50 1000 1000))
+
+(require 'package)
+(package-initialize)
+
+(if (file-exists-p (expand-file-name "config.el" user-emacs-directory))
+    (load-file (expand-file-name "config.el" user-emacs-directory))
+  (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
+
+;; Make gc pauses faster by decreasing the threshold.
+(setq gc-cons-threshold (* 2 1000 1000))
+
+;;; init.el ends here
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
