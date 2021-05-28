77c96f0404501e69cf72653afe1f2345292a00b1
Shawn Martin-Truesdell <shawntruesdell@gmail.com>
2021-05-28 12:49:02 -0700

    latest may 2021

--- init.el
+++ init.el
@@ -24,3 +24,16 @@
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(unicode-fonts exec-path-from-shell use-package-ensure-system-package dash delight use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
