1c5a0aef743ded9bdab8763689ba3757d6c91e7f
Shawn Martin-Truesdell <shawn@martin-truesdell.com>
2024-03-11 19:32:26 -0700

    latest 11-3-2024

--- lib/org-cube/org-scryfall.el
+++ lib/org-cube/org-scryfall.el
@@ -2,15 +2,29 @@
 ;;; Commentary:
 
 ;;; Code:
+(defun get-json-data-from-url (url)
+  "Retrieve JSON data from URL."
+  (with-current-buffer (url-retrieve-synchronously url t nil 10)
+    (set-buffer-multibyte t)
+    (goto-char url-http-end-of-headers)
+    (let ((parsed-json 'error))
+      (ignore-errors
+        ;; if there's a problem parsing the JSON
+        ;; parsed-json ==> 'error
+        (if (fboundp 'json-parse-buffer)
+            ;; delete everything that isn't JSON (headers)
+            (setq parsed-json (json-parse-buffer
+                               :object-type 'alist))
+          ;; Legacy :)
+          (setq parsed-json (json-read))))
+      (kill-buffer) ;; don't litter with API buffers
+      parsed-json)))
+
 (defun scryfall-card-data (card-name)
   "Retrieve card data for CARD-NAME from the Scryfall API."
-  (let* ((api-key "your-api-key-here")
-         (url (concat "https://api.scryfall.com/cards/named?exact="
-                      (url-hexify-string card-name)))
-         (response (url-retrieve-synchronously (concat url "&api_key=" api-key)))
-         (json-string (with-current-buffer response (buffer-string)))
-         (json-object (json-read-from-string json-string)))
-    json-object))
+  (let* ((url (concat "https://api.scryfall.com/cards/named?exact="
+                      (url-hexify-string card-name))))
+    (get-json-data-from-url url)))
 
 (defun scryfall-card-data-to-csv (json-object)
   "Convert the specified card JSON-OBJECT to CSV and output it in an \"org-mode\" \nsrc region."
@@ -26,15 +40,14 @@
                     (cdr (assoc 'rarity json-object))
                     (cdr (assoc 'set_name json-object))))
     (org-mode)
-    (org-ctrl-c-ctrl-c)))
+    (forward-line -1)
+    (org-ctrl-c-ctrl-c)
+    (buffer-string)))
 
 (defun scryfall-card-data-csv (card-name)
   "Retrieve card data for CARD-NAME from the Scryfall API and output it as a \nCSV in an \"org-mode\" src block, replacing the card name at the cursor."
-  (let ((csv-data (with-temp-buffer
-                    (scryfall-card-data-to-csv (scryfall-card-data card-name))
-                    (buffer-string))))
+  (let ((csv-data (scryfall-card-data-to-csv (scryfall-card-data card-name))))
     (delete-region (point) (progn (forward-word) (point)))
-    (insert (concat "#+BEGIN_SRC csv\n" csv-data "#+END_SRC"))
-    (org-ctrl-c-ctrl-c)))
+    (insert (concat "#+BEGIN_SRC csv\n" csv-data "#+END_SRC"))))
 
 ;;; org-scryfall.el ends here
